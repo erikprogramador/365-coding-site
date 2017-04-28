5394b4490255bb6b73dd3c5a364ef0b9d8d38260
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-27 22:29:55 -0300

    Add side nav

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,48 @@
 <template>
-  <div>
-    Here is the main application
+  <div class="phone-viewport">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">365 Days of code</h2>
+    </md-toolbar>
+
+    <main>
+      Here goes the main content
+    </main>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">365 Days of Code</h3>
+        </div>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <md-ink-ripple />
+          <md-icon>move_to_inbox</md-icon> <span>Inbox</span>
+        </md-list-item>
+      </md-list>
+    </md-sidenav>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    methods: {
+      toggleLeftSidenav() {
+        this.$refs.leftSidenav.toggle();
+      },
+      open(ref) {
+        console.log('Opened: ' + ref);
+      },
+      close(ref) {
+        console.log('Closed: ' + ref);
+      }
+    }
+  }
 </script>
 
 <style scoped></style>
